5703a026b72b27cd6c6aab4cf459eb863ab41fd9
HoangAnhQuan012 <kakaka021202@gmail.com>
2021-11-07 09:54:46 +0700

    bai4.js
    Làm đến Bài 4

---
+++ bai4.js
@@ -0,0 +1,15 @@
+// var fullName = 'Hoang Anh Quan';
+// var age = 19;
+// var isMale = true;
+// console.log(fullName, age, isMale);
+
+var person = {
+    Name: 'Quan',
+    Age: 19,
+    Gender: 'Male'
+};
+//person.Name = 'Hoang';
+person['Name'] = 'Hoang Anh Quan';
+console.log(person);
+// console.log(person.Name);
+// console.log(person['Age']);
